91fa219eed8c2659454f3d1962f1ecabfdf22251
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-12-22 12:00:11 +0800

    1.5

---
+++ basekball_107051053/Assets/script/score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    [Header("txet")]
+    public Text text;
+    [Header("score")]
+    public int i;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
