d450d3685c98ac24e9d840b06b9e6199a75074ee
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-12-29 11:00:59 +0800

    1.9

--- basekball_107051053/Assets/script/score.cs
+++ basekball_107051053/Assets/script/score.cs
@@ -7,9 +7,32 @@ public class score : MonoBehaviour
     public Text text;
     [Header("score")]
     public int i;
+    [Header("ball in")]
+    public int iin;
 
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        if(other.tag == "ball")
+        {
+            addi();
+        }
+
+        if(other.transform.root.name == "Player")
+        {
+            iin = 3;
+        }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform.root.name == "Player")
+        {
+            iin = 2;
+        }
+    }
+
+    private void addi()
+    {
+        i += iin;
+        text.text = "SCORE : " + i;
+    }       
 }
